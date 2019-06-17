73900f3edebaaeac60f0f9845257012bee0ce511
Mic <micangel@mail.ru>
2019-06-17 14:09:32 +0300

    pug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,118 +2,135 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { VueLoaderPlugin } = require('vue-loader')
+const {VueLoaderPlugin} = require('vue-loader')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const PATHS = {
-  src: path.join(__dirname, '../src'),
-  dist: path.join(__dirname, '../dist'),
-  assets: 'assets/'
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: 'assets/'
 }
 
 module.exports = {
-  // BASE config
-  externals: {
-    paths: PATHS
-  },
-  entry: {
-    app: PATHS.src,
-    // module: `${PATHS.src}/your-module.js`,
-  },
-  output: {
-    filename: `${PATHS.assets}js/[name].[hash].js`,
-    path: PATHS.dist,
-    publicPath: '/'
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          name: 'vendors',
-          test: /node_modules/,
-          chunks: 'all',
-          enforce: true
+    // BASE config
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src,
+        // module: `${PATHS.src}/your-module.js`,
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].[hash].js`,
+        path: PATHS.dist,
+        // publicPath: '/'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: 'vendors',
+                    test: /node_modules/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
         }
-      }
-    }
-  },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: '/node_modules/'
-    }, {
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      options: {
-        loader: {
-          scss: 'vue-style-loader!css-loader!sass-loader'
-        }
-      }
-    }, {
-      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.(png|jpg|gif|svg)$/,
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]'
-      }
-    }, {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
         }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loader: {
+                    scss: 'vue-style-loader!css-loader!sass-loader'
+                }
+            }
         }, {
-          loader: 'sass-loader',
-          options: { sourceMap: true }
-        }
-      ]
-    }, {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader',
-          options: { sourceMap: true }
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
         }, {
-          loader: 'postcss-loader',
-          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]'
+            }
+        }, {
+            test: /\.scss$/,
+            use: [
+                // 'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: `./postcss.config.js`}}
+                }, {
+                    loader: 'sass-loader',
+                    options: {sourceMap: true}
+                }
+            ]
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {sourceMap: true}
+                }, {
+                    loader: 'postcss-loader',
+                    options: {sourceMap: true, config: {path: `./postcss.config.js`}}
+                }
+            ]
+        },
+            {
+                test: /\.pug$/,
+                oneOf: [
+                    {
+                        resourceQuery: /^\?vue/,
+                        use: ["pug-plain-loader"]
+                    },
+                    {
+                        use: ["pug-loader"]
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '~': PATHS.src,
+            'vue$': 'vue/dist/vue.js',
+            images: '.src/assets/img/'
         }
-      ]
-    }]
-  },
-  resolve: {
-    alias: {
-      '~': PATHS.src,
-      'vue$': 'vue/dist/vue.js',
-    }
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[hash].css`,
-    }),
-    // Copy HtmlWebpackPlugin and change index.html for another html page
-    new HtmlWebpackPlugin({
-      template: `${PATHS.src}/index.html`,
-      filename: './index.html',
-      inject: true
-    }),
-    new CopyWebpackPlugin([
-      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
-      { from: `${PATHS.src}/static`, to: '' },
-        { from: `${PATHS.src}/${PATHS.assets}img/icons`, to: `${PATHS.assets}img/icons` }
-    ])
-  ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].[hash].css`,
+        }),
+        // Copy HtmlWebpackPlugin and change index.html for another html page
+        new HtmlWebpackPlugin({
+            // template: `${PATHS.src}/index.html`,
+            template: `${PATHS.src}/main.pug`,
+            // filename: './index.html',
+            inject: true
+        }),
+        new CopyWebpackPlugin([
+            {from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img`},
+            {from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts`},
+            {from: `${PATHS.src}/static`, to: ''},
+            {from: `${PATHS.src}/${PATHS.assets}img/icons`, to: `${PATHS.assets}img/icons`}
+        ])
+    ],
 }
